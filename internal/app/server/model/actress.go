9dfaaafffb10f924b14597a41e67e1dda8fc21ed
sssjuing <sssjuing@gmail.com>
2026-04-02 12:07:43 +0800

    x3

--- internal/app/server/model/actress.go
+++ internal/app/server/model/actress.go
@@ -6,23 +6,26 @@ import (
 	"gorm.io/gorm"
 )
 
-type Measurements struct {
-	Bust  *int // 胸围
-	Waist *int // 腰围
-	Hips  *int // 臀围
+// BWH 三围数据
+type BWH struct {
+	Bust  *int `json:"bust"`  // 胸围（单位：厘米）
+	Waist *int `json:"waist"` // 腰围（单位：厘米）
+	Hips  *int `json:"hips"`  // 臀围（单位：厘米）
 }
 
+// Actress 演员模型
 type Actress struct {
 	gorm.Model
-	Names        []*ActressName `gorm:"foreignKey:ActressID"` // 演员的所有名字
-	BirthDate    *time.Time     // 出生日期
-	BirthPlace   *string        `gorm:"size:255"` // 出生地
-	Height       *int           // 身高
-	Weight       *int           // 体重
-	Measurements Measurements   `gorm:"embedded"` // 三围数据
-	Cup          *string        `gorm:"size:2"`   // 杯型
-	BloodGroup   *string        `gorm:"size:2"`   // 血型
-	DebutDate    *time.Time     // 出道日期
-	Hobbies      *string        `gorm:"size:255"` // 爱好
-	Notes        *string        // 备注
+	UniqueName string            `gorm:"size:128;uniqueIndex;not null"` // 演员的唯一名称，用于标识演员
+	NameSets   []*ActressNameSet `gorm:"foreignKey:ActressID"`          // 演员的所有名字集（一套名字包含多种语言版本，如日文名+中文名+英文名）
+	BirthDate  *time.Time        ``                                     // 出生日期
+	BirthPlace *string           `gorm:"size:255"`                      // 出生地
+	Height     *int              ``                                     // 身高（单位：厘米）
+	Weight     *int              ``                                     // 体重（单位：公斤）
+	BWH        *BWH              `gorm:"embedded"`                      // 三围数据
+	Cup        *string           `gorm:"size:2"`                        // 杯型
+	BloodGroup *string           `gorm:"size:2"`                        // 血型
+	DebutDate  *time.Time        ``                                     // 出道日期
+	Hobbies    *string           `gorm:"size:255"`                      // 爱好
+	Notes      *string           ``                                     // 备注
 }
--- internal/app/server/model/video.go
+++ internal/app/server/model/video.go
@@ -7,18 +7,19 @@ import (
 	"gorm.io/gorm"
 )
 
+// Video 视频模型
 type Video struct {
 	gorm.Model
-	SerialNumber string          `gorm:"size:128;uniqueIndex;not null"`
-	CoverPath    string          `gorm:"size:1024;not null"` // 封面在对象存储中桶内的路径
-	Title        *string         `gorm:"size:512;index"`
-	ChineseTitle *string         `gorm:"size:512;index"`
-	ActressNames []*ActressName  `gorm:"many2many:actress_name_video"` // 通过演员名字关联
-	ReleaseDate  *time.Time      // 发行日期
-	VideoPath    *string         `gorm:"size:1024"` // 视频在对象存储中桶内的路径
-	Mosaic       *bool           // 是否打马赛克
-	Tags         *datatypes.JSON // 标签，JSON 数组格式存储
-	Synopsis     *string         // 概要
-	M3u8URL      *string         `gorm:"size:1024"` // M3U8 播放列表 URL
-	M3u8Referer  *string         `gorm:"size:1024"` // M3U8 引用来源
+	SerialNumber    string            `gorm:"size:128;uniqueIndex;not null"`    // 视频序列号，唯一标识
+	CoverPath       string            `gorm:"size:1024;not null"`               // 封面在对象存储中桶内的路径
+	Title           *string           `gorm:"size:512;index"`                   // 视频原标题
+	ChineseTitle    *string           `gorm:"size:512;index"`                   // 视频中文标题
+	ActressNameSets []*ActressNameSet `gorm:"many2many:video_actress_name_set"` // 视频关联的演员名字集（同一个演员在不同的作品中可能使用不同的名字）
+	ReleaseDate     *time.Time        ``                                        // 发行日期
+	VideoPath       *string           `gorm:"size:1024"`                        // 视频在对象存储中桶内的路径
+	Mosaic          *bool             ``                                        // 是否打马赛克
+	Tags            *datatypes.JSON   ``                                        // 标签，JSON 数组格式存储
+	Synopsis        *string           ``                                        // 概要
+	M3u8URL         *string           `gorm:"size:1024"`                        // M3U8 播放列表 URL
+	M3u8Referer     *string           `gorm:"size:1024"`                        // M3U8 引用来源
 }
